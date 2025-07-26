46b1e3280dcb74912d8f242d56c95605519041a5
Musa-Mahmoud <mosa.mahmoud87@gmail.com>
2025-07-27 02:00:59 +0300

    Feat: FreeRTOS ported to all features

--- Features/Blinker_Toggle_Switch_Control/Core/Inc/main.h
+++ Features/Blinker_Toggle_Switch_Control/Core/Inc/main.h
@@ -31,6 +31,8 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "FreeRTOS.h"
+#include "task.h"
 
 /* USER CODE END Includes */
 
